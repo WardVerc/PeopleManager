8c62dbe63c5a0c9b5da740ad233b9555cd6af388
Ward Vercruyssen <r0435477@student.vives.be>
2021-02-12 22:56:17 +0100

    Les 2 Oefening 1 klaar

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleManager.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PeopleManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
