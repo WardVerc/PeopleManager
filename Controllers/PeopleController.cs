145a1e1e1134b50a800a1d05296aa1966e39adf7
Ward Vercruyssen <r0435477@student.vives.be>
2021-02-14 16:49:29 +0100

    Les 5 Oefening 1 klaar. Create, Edit en Delete -functionaliteiten toegevoegd. Enkel layout knoppen moet nog beter.

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using PeopleManager.Core;
 using PeopleManager.Models;
@@ -13,6 +14,8 @@ namespace PeopleManager.Controllers
         {
             _database = database;
         }
+        
+        [HttpGet]
         public IActionResult Index()
         {
             var personen = GetPeople();
@@ -24,5 +27,74 @@ namespace PeopleManager.Controllers
         {
             return _database.People;
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            person.Id = GetNewId();
+            _database.People.Add(person);
+
+            return RedirectToAction("Index");
+        }
+
+        private int GetNewId()
+        {
+            if (_database.People.Any())
+            {
+                var getMaxId = _database.People.Max(p => p.Id);
+                return getMaxId += 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var databasePerson = _database.People.SingleOrDefault(p => p.Id == id);
+
+            return View(databasePerson);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Person person)
+        {
+            var databasePerson = _database.People.SingleOrDefault(p => p.Id == person.Id);
+
+            if (databasePerson == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                databasePerson.Naam = person.Naam;
+                databasePerson.Adres = person.Adres;
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        [Route("People/Delete/{id?}")]
+        public IActionResult Delete(int id)
+        {
+            var databasePerson = _database.People.SingleOrDefault(p => p.Id == id);
+            if (databasePerson == null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            _database.People.Remove(databasePerson);
+            return RedirectToAction("Index");
+        }
+        
     }
 }
